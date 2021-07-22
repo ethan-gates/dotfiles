b7c01dcadd4eb0d422605d9da15d74d31aedbfb8
Ethan Gates <ethan@stonegate.org>
2021-07-22 15:10:03 -0700

    Kitty script fixes + kitty aesthetics
    -no title bar
    -goodbye opacity =(

--- shell-scripts/kitty.zsh
+++ shell-scripts/kitty.zsh
@@ -8,17 +8,17 @@ precmd () {print -Pn "\e]0;%1d\a"}
 
 # Change the theme for kitty provided kitty-themes
 function kittytheme() {
-    THEME_PATH=$DOTFILES_SRC/kitty-themes/themes/$1.conf
+    THEME_PATH=$DOTFILES_SRC/kitty/kitty-themes/themes/$1.conf
     echo "Looking for $THEME_PATH"
     echo "\$1 = $1"
     if [[ -e $THEME_PATH ]]; then 
         rm $DOTFILES_SRC/kitty/theme.conf
-        ln -s $THEME_PATH ~/.config/kitty/theme.conf
+        ln -s $THEME_PATH $DOTFILES_SRC/kitty/theme.conf
     else
         echo "$1 Theme not found"
     fi
 }
 
 function kittythemelist() {
-    ls $DOTFILES_SRC/kitty-themes/themes
+    ls $DOTFILES_SRC/kitty/kitty-themes/themes
 }
